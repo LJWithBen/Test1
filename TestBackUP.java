c26ad753a9e5729a91677d6ce0cd8f19b82b38bd
Despair-lj <691287671@qq.com>
2014-09-10 14:36:13 +0800

    20140910 -false
    the new file,I think it is very failure

--- TestBackUP.java
+++ TestBackUP.java
@@ -6,13 +6,21 @@ abstract class Shape{
 class Rectangular extends Shape{
 	private String str1 ;
 	private String str2 ;
-	private double x = Double.parseDouble(str1);
-	private double y = Double.parseDouble(str2);
+	private double x ;
+	private double y ;
+	public double calcx(){
+		x = Double.parseDouble(str1);
+		return x;
+	}
+	public double calcy(){
+		y = Double.parseDouble(str2);
+		return y;
+	}
 	public double area(){
-		return x*y;
+		return this.calcx()*this.calcy();
 	}
 	public double perimeter(){
-		return 2*(x+y);
+		return 2*(this.calcx()+this.calcy());
 	}
 	public Rectangular(String str1,String str2){
 		this.str1 = str1;
@@ -24,34 +32,56 @@ class Triangle extends Shape{
 	private String str1 ;
 	private String str2;
 	private String str3;
-	private double x = Double.parseDouble(str1);
-	private double y = Double.parseDouble(str2);
-	private double z = Double.parseDouble(str3);
-	private double p = (x+y+z)/2;
-	public double area(){
-		return Math.sqrt(p*(p-x)*(p-y)*(p-z));
-	}
-	public double perimeter(){
-		return x+y+z;
-	}
+	private double x ;
+	private double y ;
+	private double z ;
+	private double p ;
 	public Triangle(String str1,String str2,String str3){
 		this.str1 = str1;
 		this.str2 = str2;
 		this.str3 = str3;
 	}
+	public double calcx(){
+		x = Double.parseDouble(str1);
+		return x;
+	}
+	public double calcy(){
+		y = Double.parseDouble(str2);
+		return y;
+	}
+	public double calcz(){
+		z = Double.parseDouble(str3);
+		return z;	
+	}
+	public double calc(){
+		p = (this.calcx()+this.calcy()+this.calcz())/2;
+		return p;
+	}
+	public double area(){
+		return Math.sqrt(this.calc()*((this.calc())-this.calcx())*((this.calc())-this.calcy())*((this.calc())-this.calcz()));
+	}
+	public double perimeter(){
+		return this.calc()*2;
+	}
+
 }
 
 class Circle extends Shape{
-	private String str;
+	private String str ;
 	public Circle(String str){
 		this.str = str;
+		System.out.println(this.str);
 	}
-	double r = Double.parseDouble(str);
+	private double r ;
+	public double calc(){
+		r = Double.parseDouble(this.str);
+		return r;
+	}
 	public double area(){
-		return 3.14*r*r;
+		return 3.14*this.calc()*this.calc();
 	}
 	public double perimeter(){
-		return 2*3.14*r;
+		return 2*3.14*this.calc();
 	}
 	
 }
@@ -66,36 +96,49 @@ class Operate{
 			System.out.println("ERROR,please input ONE to THREE number");
 			System.exit(1);
 		}else if (info.length==1){
-			System.out.println(login(info[0]));
 			if (login(info[0])){
-				Circle c = new Circle(info[0]);
-			
-			//Shape s =new Circle(info[0]);
-			//Circle c = (Circle)s;
-			System.out.println("This is a circle and its area is "+c.area()+"\t"+"its perimeter is "+c.perimeter());
+				Shape s =new Circle(info[0]);
+				Circle c = (Circle)s;
+				System.out.println("This is a circle and its area is "+c.area()+"\t"+"its perimeter is "+c.perimeter());
 			}else {
-				System.out.println("----------------Test------------");
+				System.out.println("ERROR,please input ONE to THREE number-------1");
+				System.exit(1);
 			}
-		}/*else{
-			System.out.println("ERROR,please input ONE to THREE number-------1");
-			System.exit(1);
-		} */
-		else if (info.length==2&&login(info[0])&&login(info[1])){
-			Shape s = new Rectangular(info[0],info[1]);
-			Rectangular r = (Rectangular)s;
-			System.out.println("This is a Rectangular and its area is "+r.area()+"\t"+"its perimeter is "+r.perimeter());
-		} /*else{
-			System.out.println("ERROR,please input ONE to THREE number-------2");
-			System.exit(1);
-		} */
-		else  if(info.length==3&&login(info[0])&&login(info[1])&&login(info[2])){
-			Shape s = new Triangle(info[0],info[1],info[3]);
-			Triangle t = (Triangle)s;
-			System.out.println("This is a Triangle and its area is "+t.area()+"\t"+"its perimeter is" +t.perimeter());
-		}/*else{
+		}else if (info.length==2){
+				if(login(info[0])){
+					if(login(info[1])){
+						Shape s = new Rectangular(info[0],info[1]);
+						Rectangular r = (Rectangular)s;
+						System.out.println("This is a Rectangular and its area is "+r.area()+"\t"+"its perimeter is "+r.perimeter());
+					}else {
+						System.out.println("ERROR,please input ONE to THREE number-------2");
+						System.exit(1);
+					}
+				}else{
+					System.out.println("ERROR,please input ONE to THREE number-------2");
+					System.exit(1);
+				}
+			}else  if(info.length==3){
+			if (login(info[0])){
+				if(login(info[1])){
+					if(login(info[2])){
+						Shape s = new Triangle(info[0],info[1],info[2]);
+						Triangle t = (Triangle)s;
+						System.out.println("This is a Triangle and its area is "+t.area()+"\t"+"its perimeter is " +t.perimeter());
+		}else{
 			System.out.println("ERROR,please input ONE to THREE number---------3");
 			System.exit(1);
-		}*/ 
+		}
+					}else{
+						System.out.println("ERROR,please input ONE to THREE number---------3");
+						System.exit(1);
+					}}else{
+						System.out.println("ERROR,please input ONE to THREE number---------3");
+						System.exit(1);
+					}}else{
+						System.out.println("ERROR,please input ONE to THREE number---------3");
+						System.exit(1);
+					}
 	}
 	public boolean login(String str){
 		char a = 48;
@@ -116,18 +159,12 @@ class Operate{
 }
 
 
-public class TestBackUP{
+public class TestBackUp{
 	public static void main(String[]agre){
 		Operate o = new Operate(agre);
-		o.judge();
-		
-	}
-		
-	}
-	/*public static void judge(){
-		
-		//Rectangular,Triangle,Circle
-	}*/
+		o.judge();	
+	}	
+}
 
 
 
